6c26152c494cec901c018e96e62738d61fea29f4
SillyDev2026 <kramerlance797@gmail.com>
2026-06-11 23:56:16 -0800

    Sync from Roblox Studio — Updated by sillydev0050 (Open Source Enabled)

--- GUI/ScreenGui/LocalScript.lua
+++ GUI/ScreenGui/LocalScript.lua
@@ -1,8 +1,1 @@
-local Test = Instance.new('TextButton')
-Test.Parent = script.Parent
-Test.Name = 'Test'
-Test.Visible = true
-Test.Text = 'Test'
-Test.Size = UDim2.new(0, 100, 0, 50)
-Test.Position = UDim2.fromScale(0.5, 0.5)
-Test.AnchorPoint = Vector2.new(0.5, 0.5)
+local test = 'wow'
